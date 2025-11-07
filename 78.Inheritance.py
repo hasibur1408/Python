b7ce1592ae4353728ce56c4104fed11dde6666f2
hasibur1408 <1903124@ritindia.edu>
2025-11-07 15:51:52 +0600

    Add files via upload

---
+++ 78.Inheritance.py
@@ -0,0 +1,18 @@
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+    def fare(self):
+        return self.capacity * 100
+
+class Bus(Vehicle):
+    def __init__(self, name, mileage, capacity):
+        super().__init__(name, mileage, capacity)
+    def fare(self):
+        total_fare=self.capacity * 100
+        return total_fare + (total_fare*0.1)
+
+School_bus = Bus("School Volvo", 12, 50)
+print("Total Bus fare is:", School_bus.fare())
